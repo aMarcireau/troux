fa4150c08632bfb0da0b4c3191709a5c16b56c25
GepNX <clement.ricateau@hotmail.fr>
2014-09-15 23:14:44 +0200

    Scripting modification to adapt to the new classes
    Scripting modification to adapt to the new class structure

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from Chromosom import Chromosom
-from FatherBeing import FatherBeing
-from ChildBeing import ChildBeing
+from Being import Being
+
 
 gingerChromosom = Chromosom(
     number = 1,
@@ -22,13 +22,33 @@ blackHairedChromosom = Chromosom(
     gene = "blackHaired"
 )
 
-originalGinger = FatherBeing("Ronald", [gingerChromosom])
-originalSlut = FatherBeing("P!nk", [blackHairedChromosom])
-
-
-print(
-    originalGinger.geneticMix([
-        originalGinger.getChromosomsSet(), 
-        originalSlut.getChromosomsSet()
-    ])[0].getGene()
+blondHairedChromosom = Chromosom(
+    number = 1,
+    gene = "blondHaired"
 )
+
+xChromosom = Chromosom(
+    number = 2,
+    gene = "X chromosom")
+
+yChromosom = Chromosom(
+    number = 2,
+    gene = "Y chromosom")
+
+originalGinger = Being("Joseph the blackhaired", [gingerChromosom, blackHairedChromosom, yChromosom, xChromosom])
+originalSlut = Being("Jackie's mother the ginger", [blackHairedChromosom, blackHairedChromosom, xChromosom, xChromosom])
+originalFucker = Being("Zboub the blondhaired", [blondHairedChromosom, blondHairedChromosom, xChromosom, yChromosom])
+
+Jesus = originalGinger.mate(originalSlut, "Jesus")
+Marie = Jesus.mate(originalFucker, "Marie")
+
+#print(test.getName)
+print(
+    "The child of ", originalGinger.name
+    , " with ", originalSlut.name
+    , " is named "
+    ,Jesus.name
+    , "and it is "
+    , Jesus.getChromosomsSet()[1][0].getGene()
+    , Jesus.getChromosomsSet()[1][1].getGene()
+    )
